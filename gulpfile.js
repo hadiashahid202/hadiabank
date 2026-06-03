b302770371b7244c46021c099ada82b7cfd26684
Hadia Shahid <hadiashahid202@gmail.com>
2026-06-03 21:10:33 +0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,12 @@ gulp.task("create-zip", function () {
     .pipe(zip.default("hadiabank.zip")) // ← change zip() to zip.default()
     .pipe(gulp.dest("./"));
 });
+
+// gulp.task("create-zip", function(){
+//   console.log("creating zip file");
+// }
+// );
+// function createZipTask(){
+//   console.log("creating zip file");
+// }
+// gulp.task("create-zip",createZipTask);
