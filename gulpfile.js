44e1aeef504a369ce9fe91434452fcc6fe2b353a
Hadia Shahid <hadiashahid202@gmail.com>
2026-06-05 20:56:13 +0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
+// const gulp = require("gulp");
+// const zip = require("gulp-zip");
+
+// gulp.task("create-zip", function () {
+//   return gulp
+//     .src(["index.html", "style.css", "index.js", "assets/**/*"])
+//     .pipe(zip.default("hadiabank.zip")) // ← change zip() to zip.default()
+//     .pipe(gulp.dest("../"));
+// });
+
 const gulp = require("gulp");
 const zip = require("gulp-zip");
 
-gulp.task("create-zip", function () {
+function zipFileTask() {
   return gulp
     .src(["index.html", "style.css", "index.js", "assets/**/*"])
-    .pipe(zip.default("hadiabank.zip")) // ← change zip() to zip.default()
-    .pipe(gulp.dest("./"));
-});
-
-// gulp.task("create-zip", function(){
-//   console.log("creating zip file");
-// }
-// );
-// function createZipTask(){
-//   console.log("creating zip file");
-// }
-// gulp.task("create-zip",createZipTask);
+    .pipe(zip.default("hadiabank.zip"))
+    .pipe(gulp.dest("../"));
+}
+gulp.task("create-zip", zipFileTask);
---
+++ js/conditional.js
@@ -0,0 +1,80 @@
+let num1 = 10;
+if (num1 > 0) {
+  console.log(num1 + " is Positive");
+} else if (num1 < 0) {
+  console.log(num1 + " is Negative");
+} else {
+  console.log("Number is Zero");
+}
+
+let num2 = -5;
+if (num2 > 0) {
+  console.log(num2 + " is Positive");
+} else if (num2 < 0) {
+  console.log(num2 + " is Negative");
+} else {
+  console.log("Number is Zero");
+}
+
+let score1 = 75;
+let result1 = score1 >= 50 ? "Pass" : "Fail";
+console.log("Score:", score1, "-", result1);
+
+let score2 = 40;
+let result2 = score2 >= 50 ? "Pass" : "Fail";
+console.log("Score:", score2, "-", result2);
+
+let day1 = 1;
+switch (day1) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid Day");
+}
+
+let day2 = 7;
+
+switch (day2) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid Day");
+}
